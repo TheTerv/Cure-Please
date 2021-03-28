a029550f7fd98bc8c00342d7c6225f7978aa1564
unknown <vivi.orunitia@gmail.com>
2021-03-28 09:23:32 -0300

    Fixed Project locations

---
+++ CurePlease.Models/Constants/Spells.cs
@@ -0,0 +1,113 @@
+﻿
+
+namespace CurePlease.Models.Constants
+{
+    public static class Spells
+    {
+        public static string Unknown = string.Empty;
+
+        public static string Cure = "Cure";
+        public static string Cure_II = "Cure II";
+        public static string Cure_III = "Cure III";
+        public static string Cure_IV = "Cure IV";
+        public static string Cure_V = "Cure V";
+        public static string Cure_VI = "Cure VI";
+        public static string FullCure = "Full Cure";
+
+        public static string Regen = "Regen";
+        public static string Regen_II = "Regen II";
+        public static string Regen_III = "Regen III";
+        public static string Regen_IV = "Regen IV";
+        public static string Regen_V = "Regen V";
+
+        public static string Curaga = "Curaga";
+        public static string Curaga_II = "Curaga II";
+        public static string Curaga_III = "Curaga III";
+        public static string Curaga_IV = "Curaga IV";
+        public static string Curaga_V = "Curaga V";
+
+        public static string Protect = "Protect";
+        public static string Protect_II = "Protect II";
+        public static string Protect_III = "Protect III";
+        public static string Protect_IV = "Protect IV";
+        public static string Protect_V = "Protect V";
+
+        public static string Protectra = "Protectra";
+        public static string Protectra_II = "Protectra II";
+        public static string Protectra_III = "Protectra III";
+        public static string Protectra_IV = "Protectra IV";
+        public static string Protectra_V = "Protectra V";
+
+        public static string Reraise = "Reraise";
+        public static string Reraise_II = "Reraise II";
+        public static string Reraise_III = "Reraise III";
+        public static string Reraise_IV = "Reraise IV";
+
+        public static string Shell = "Shell";
+        public static string Shell_II = "Shell II";
+        public static string Shell_III = "Shell III";
+        public static string Shell_IV = "Shell IV";
+        public static string Shell_V = "Shell V";
+
+        public static string Shellra = "Shellra";
+        public static string Shellra_II = "Shellra II";
+        public static string Shellra_III = "Shellra III";
+        public static string Shellra_IV = "Shellra IV";
+        public static string Shellra_V = "Shellra V";
+
+        public static string Refresh = "Refresh";
+        public static string Refresh_II = "Refresh II";
+        public static string Refresh_III = "Refresh III";
+
+        public static string Haste = "Haste";
+        public static string Haste_II = "Haste_II";
+        public static string Adloquium = "Adloquium";
+        public static string Flurry = "Flurry";
+        public static string Flurry_II = "Flurry_II";
+        public static string Phalanx = "Phalanx";
+        public static string Phalanx_II = "Phalanx II";
+        public static string Blink = "Blink";
+        public static string Aquaveil = "Aquaveil";
+        public static string Stoneskin = "Stoneskin";
+        public static string Klimaform = "Klimaform";
+        public static string Temper = "Temper";
+        public static string Blaze_Spikes = "Blaze Spikes";
+        public static string Shock_Spikes = "Shock Spikes";
+        public static string Ice_Spikes = "Ice Spikes";
+        public static string Auspice = "Auspice";
+        public static string Sacrifice = "Sacrifice";
+        public static string Sneak = "Sneak";
+        public static string Invisible = "Invisible";
+
+        public static string Boost_STR = "Boost-STR";
+        public static string Boost_DEX = "Boost-DEX";
+        public static string Boost_VIT = "Boost-VIT";
+        public static string Boost_AGI = "Boost-AGI";
+        public static string Boost_INT = "Boost-INT";
+        public static string Boost_MND = "Boost-MND";
+        public static string Boost_CHR = "Boost-CHR";
+
+        public static string Gain_STR = "Gain-STR";
+        public static string Gain_DEX = "Gain-DEX";
+        public static string Gain_VIT = "Gain-VIT";
+        public static string Gain_AGI = "Gain-AGI";
+        public static string Gain_INT = "Gain-INT";
+        public static string Gain_MND = "Gain-MND";
+        public static string Gain_CHR = "Gain-CHR";
+
+        public static string Cursna = "Cursna";
+        public static string Paralyna = "Paralyna";
+        public static string Poisona = "Poisona";
+        public static string Blindna = "Blindna";
+        public static string Viruna = "Viruna";
+        public static string Silena = "Silena";
+        public static string Stona = "Stona";
+        public static string Esuna = "Esuna";
+        public static string Erase = "Erase";
+
+        public static string Cura = "Cura";
+
+        public static string Utsusemi_Ichi = "Utsusemi: Ichi";
+        public static string Utsusemi_Ni = "Utsusemi: Ni";
+    }
+}
